c3b555b70fa86223f7d1acd6cf0788116b5fde99
Moises.alves <Moises.alves@atp.com.br>
2022-12-26 18:43:20 -0300

    Criada tabuada

---
+++ wfTabuada.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="wfTabuada.aspx.cs" Inherits="waConhecendoOsComponentes.wfTabuada" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+          <h1>Tabuada</h1>
+        </div>
+        <asp:DropDownList ID="dlNumeros" runat="server">
+        </asp:DropDownList>
+        <asp:Button ID="btExecutar" runat="server" OnClick="btExecutar_Click" Text="Exibir a tabuada" />
+        <br />
+        <br />
+        <asp:ListBox ID="lbDados" runat="server" Height="287px" Width="1101px"></asp:ListBox>
+    </form>
+</body>
+</html>
---
+++ wfTabuada.aspx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace waConhecendoOsComponentes
+{
+    public partial class wfTabuada : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                for (int i = 1; i < 11; i++)
+                {
+                    dlNumeros.Items.Add(i.ToString());
+                }
+            }
+        }
+
+        protected void btExecutar_Click(object sender, EventArgs e)
+        {
+            lbDados.Items.Clear();
+            ListItem li = dlNumeros.SelectedItem;
+            int n = Convert.ToInt32(li.Value);
+            int t = 0;
+            for (int i = 0; i <= 10; i++)
+            {
+                t = i * n;
+                lbDados.Items.Add(i.ToString() + " x " + n.ToString() + " = " + t.ToString());
+            }
+        }
+    }
+}
